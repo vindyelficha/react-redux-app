dab6bd6a991835a442b3127d32a4a3c2c9fc5233
vindyelficha <vindyelficha98@gmail.com>
2021-05-17 09:56:34 +0700

    menambahkan component auth (signin dan signup) di sisi frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React,{Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Navbar from './components/layout/Navbar'
-import Dashboard from './components/dashboard/Dashboard'
-import ProjectDetails from './components/projects/ProjectDetails'
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route exact path='/' component={Dashboard}/> 
           {/* exact biar ngeroute saat bener2 ditype / aja  */}
           <Route path='/project/:id' component={ProjectDetails}/>
+          <Route path='/signin' component={SignIn}/>
+          <Route path='/signup' component={SignUp}/> 
         </Switch>
       </div>
     </BrowserRouter>
