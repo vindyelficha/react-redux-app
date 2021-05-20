dc85e69a21b9d8de2cd0bc983882df9fcbfda831
vindyelficha <vindyelficha98@gmail.com>
2021-05-20 14:32:06 +0700

    menampilkan detail project

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,21 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 const ProjectDetails = (props) =>{
-    const id = props.match.params.id;
+    const { project } = props;
+    
+    if (project){
     return(
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project Title - {id}</span>
-                    <p>lorem ipsum ejnvr wkehvnkwej wukehvnrkj uskehnvkws uksvnkwr siehnvwke iaejvil euhvik wihvnk wenvkwe wekvjwoe9a eiwjwoaljc wiehvnwelvwe weihvwoelw vwioejvwilek</p>
+                    <span className="card-title">{project.title}</span>
+                    <p>{project.content}</p>
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by Vindy</div>
+                    <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
                     <div>17 Mei 2021, 9AM</div>
                 </div>
             </div>
         </div>
-    )
+    )}else{
+    return(
+        <div className="container center">
+            <p>Loading project...</p>
+        </div>
+        )
+    }
 }
 
-export default ProjectDetails;
+const mapStateToProps = (state, ownProps) => {
+    console.log(state);
+    const id = ownProps.match.params.id
+    const projects = state.firestore.data.projects
+    const project = projects ? projects[id] : null
+    return {
+        project: project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection : 'projects'}
+    ])
+)(ProjectDetails);
