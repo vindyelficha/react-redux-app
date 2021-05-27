739531a62bd98f6ef7e26a34ac5b91d6aa839c44
vindyelficha <vindyelficha98@gmail.com>
2021-05-27 08:12:49 +0700

    cara agar navbar ditampilan sesuai user signout atau signin

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,22 +4,23 @@ import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux';
 
-const Navbar =() =>{
+const Navbar =(props) =>{
+    const { auth } = props;
+    console.log(auth);
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks /> ;
     return(
         <nav className ="nav.nav-wrapper.blue.darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">MarioPlan</Link>
-                <SignedInLinks/>
-                <SignedOutLinks/>
+                { links }
             </div>
         </nav>
     )
 }
 
-const mapStateToProps = (state, ownProps) => {
-    console.log(state);
+const mapStateToProps = (state) => {
     return {
-        
+        auth: state.firebase.auth
     }
 }
 
